df7e233e84e8cc40a24964bb9fb9534a075c2519
PicuBicu <championello@gmail.com>
2022-03-11 13:20:29 +0100

    Add about activity

--- app/src/main/java/pl/picubicu/calculator/MainActivity.java
+++ app/src/main/java/pl/picubicu/calculator/MainActivity.java
@@ -2,13 +2,25 @@ package pl.picubicu.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button aboutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handleAboutActivity();
+    }
+
+    private void handleAboutActivity() {
+        this.aboutButton = findViewById(R.id.aboutButton);
+        this.aboutButton.setOnClickListener((view) -> {
+            startActivity(new Intent(MainActivity.this, AboutActivity.class));
+        });
     }
 }
